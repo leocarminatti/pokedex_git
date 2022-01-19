e6f49e4459917254607e33a7b77a390ded6fa7c3
Love People <lovepeople@Leonardos-MacBook-Pro.local>
2022-01-18 21:08:41 -0300

    remover variavel name

--- lib/home.dart
+++ lib/home.dart
@@ -6,7 +6,6 @@ class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
-  final String name;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
